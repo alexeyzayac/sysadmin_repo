e4a0843f7b69b06e50f6020a14737ecf77024946
alexeyzayac <alexeyzayac@icloud.com>
2025-08-28 14:45:14 +0300

    4 commit

--- linux_gaid/aliace.sh
+++ linux_gaid/aliace.sh
@@ -1,3 +1,27 @@
-
-
-alias ll='ls -AlFh --color'
+# ==================== АЛИАСЫ ====================
+# алиасы для ls
+alias ll='ls -AlhF --color'
+alias la='ls -A'
+alias l='ls -CF'
+# алиасы для навигации
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+# безопасные алиасы
+alias rm='rm -I'
+alias cp='cp -i'
+alias mv='mv -i'
+alias mkdir='mkdir -p'
+# служебные алиасы
+alias c='clear'
+alias dhh='df -h -x tmpfs -x efivarfs'
+alias wget='wget -c'
+alias untar='tar -xzf'
+alias myip='curl http://ipecho.net/plain; echo'
+# алиасы для git
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gpush='git push'
+alias gpull='git pull'
+alias gl='git log --oneline --graph --decorate'
